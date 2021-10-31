2c0a4468336c22a2039ec6a889fed1dd40c6a9e8
amran5 <amran40004@gmail.com>
2021-10-31 15:56:00 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qpwhn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("foodelix");
+        const servicesCollection = database.collection("services");
+
+        //GET API 
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        //GET SINGLE SERVICE API
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObJectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service)
+        });
+
+        // POST API
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log('post hitting')
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result);
+        });
+
+        // DELETE API
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObJectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            console.log(result)
+            res.json(result);
+        });
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Foodelex Server Running')
+});
+
+app.listen(port, () => {
+    console.log('port here listen', port)
+});
